04fcf3d02f464dad8aec34b32a9872809c1e6542
gorkama <gorka16@hotmail.com>
2017-12-11 12:28:54 +0100

    desapare boton

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
     Usuarios Aleatorios
 </div>
 <div class="container">
-<form action="">
-    <button class="btn btn-primary btn-lg btn-block">enseñar</button>
+<form action="#">
+    <button class="btn btn-primary btn-lg btn-block" id="boton">enseñar</button>
 </form>
 </div>
 <!-- Optional JavaScript -->
@@ -24,5 +24,14 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<!--<script src="../../usuarios%20aleatorios/usuarios-aleatorios/js/jquery-3.2.1.min.js"></script>-->
+<script>
+    $(function(){
+        $("#boton").on("click",function () {
+            $(this).css({"display": "none"})
+        });
+    })
+</script>
+
 </body>
 </html>
